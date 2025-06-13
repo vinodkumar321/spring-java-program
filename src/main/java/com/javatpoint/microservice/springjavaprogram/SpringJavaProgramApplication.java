3d27e4e7cc25eccf6b96d747a040ce420e26f7e7
vinodkumar321 <vk2830678@gmail.com>
2025-06-13 14:58:21 +0530

    Initial commit

---
+++ src/main/java/com/javatpoint/microservice/springjavaprogram/SpringJavaProgramApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.microservice.springjavaprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaProgramApplication.class, args);
+    }
+
+}
